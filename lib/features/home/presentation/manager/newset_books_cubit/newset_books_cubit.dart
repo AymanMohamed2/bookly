4b6c58d724c0564b9c3ba6da548a1444d304a085
darknayaam <darknaya.am@gmail.com>
2023-02-22 12:50:44 +0200

    create newset books cubit state

---
+++ lib/features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newset_books_state.dart';
+
+class NewsetBooksCubit extends Cubit<NewsetBooksState> {
+  NewsetBooksCubit() : super(NewsetBooksInitial());
+}
